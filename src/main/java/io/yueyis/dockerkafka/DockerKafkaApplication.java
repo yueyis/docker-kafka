31589a32ed314d4ecbcacb8a910bad07987d95f3
yueyis <zju3757@gmail.com>
2020-12-07 23:50:00 +0800

    init commit

---
+++ src/main/java/io/yueyis/dockerkafka/DockerKafkaApplication.java
@@ -0,0 +1,13 @@
+package io.yueyis.dockerkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yueyis/dockerkafka/DockerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yueyis.dockerkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
